934d6d17e6586038931e797421ec390ca0cb8085
Spencer Sellers <ssellers@atlasrfid.com>
2017-11-14 07:55:04 -0600

    Generalize future nodes

--- lib/Search.hs
+++ lib/Search.hs
@@ -2,6 +2,7 @@
 module Search where
 
 import Control.Lens
+import Data.List as List
 
 data Move s a = Move s a deriving Show
 
@@ -17,17 +18,28 @@ makeLenses ''Search
 instance (Show s, Show a) => Show (Search s a) where
     show s = "Search " ++ show  (s ^. state)
 
-type SearchCombiner s a = [Search s a] -> [Search s a] -> [Search s a]
+class FutureNodes n where
+    next :: n a -> Maybe (a, n a)
+    addNodes :: [a] -> n a -> n a
 
+data SimpleSearchType = BFS | DFS;
+data SimpleNodes a = SimpleNodes SimpleSearchType [a]
+
+instance FutureNodes SimpleNodes where
+    next (SimpleNodes dir nodes) = List.uncons nodes & _Just . _2 %~ SimpleNodes dir
+    addNodes new (SimpleNodes DFS old) = SimpleNodes DFS $ new ++ old
+    addNodes new (SimpleNodes BFS old) = SimpleNodes BFS $ old ++ new
+    
 bfs, dfs :: Search s a -> Maybe [Move s a]
-bfs search = coreSearch (\new old -> old ++ new) [search]
-dfs search = coreSearch (\new old -> new ++ old) [search]
+bfs search = coreSearch $ SimpleNodes BFS [search]
+dfs search = coreSearch $ SimpleNodes DFS [search]
 
-coreSearch :: SearchCombiner s a -> [Search s a] -> Maybe [Move s a]
-coreSearch combiner (search:rest) = if (search ^. win) (search ^. state) 
-    then Just $ search ^. history
-    else coreSearch combiner $ combiner (expand search) rest
-coreSearch _ [] = Nothing
+coreSearch :: FutureNodes f => f (Search s a) -> Maybe [Move s a]
+coreSearch nodes = case next nodes of
+    Just (search, rest) -> if (search ^. win) (search ^. state) 
+        then Just $ search ^. history
+        else coreSearch $ addNodes (expand search) rest
+    Nothing -> Nothing
 
 applyMove :: Search s a -> Move s a -> Search s a
 applyMove search move@(Move new' action') = search 
