d44eef5cca3e20a7db6bb69951c5d1c1615fe6b2
Spencer Sellers <ssellers@atlasrfid.com>
2017-11-30 23:43:23 -0600

    Refactor

--- advents/2017/Advent1.hs
+++ advents/2017/Advent1.hs
@@ -9,13 +9,16 @@ one nums = one' nums'
     where nums' = nums ++ [head nums]
 
 one' :: [Int] -> Int 
-one' (x:next:xs) = if x == next then x + (one' (next:xs)) else one' (next:xs)
+one' (x:next:xs) 
+    | x == next = x + rest
+    | otherwise = rest
+    where rest = one' (next:xs)
 one' _ = 0
 
 two :: [Int] -> Int
 two l = sum . map (comp . dual) $ [0..length l-1]
     where dual i = (l !! i, l !! halfIndex i)
-          halfIndex i = (i + (length l) `div` 2) `mod` length l
+          halfIndex i = (i + length l `div` 2) `mod` length l
           comp (a, b) = if a == b then a else 0
 
 main :: IO ()
