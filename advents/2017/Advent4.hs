b2b392079afe663562c07c5a722f10eae55e3942
Spencer Sellers <ssellers@atlasrfid.com>
2017-12-04 08:37:53 -0600

    Replace splitBy with words

--- advents/2017/Advent4.hs
+++ advents/2017/Advent4.hs
@@ -1,6 +1,5 @@
 module Main where
 import AdventLib    
-import Data.List.Split
 import Data.List
 
 hasDuplicates :: Eq a => [a] -> Bool
@@ -8,7 +7,7 @@ hasDuplicates l = nub l /= l
 
 main :: IO ()
 main = do
-    wordLines <- (fmap . fmap) (splitOn " ") inputLines
+    wordLines <- (fmap . fmap) words inputLines
     let solve = print . length . filter (not . hasDuplicates)
     solve wordLines
     solve $ (fmap . fmap) sort wordLines
