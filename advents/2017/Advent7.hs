1adfe5a79d93734ac5aeec36309c8c0a5856b8f9
Spencer Sellers <ssellers@atlasrfid.com>
2017-12-08 11:15:30 -0600

    More documentation

--- advents/2017/Advent7.hs
+++ advents/2017/Advent7.hs
@@ -6,12 +6,14 @@ import Parsing
 import Text.Parsec
 import Data.Maybe
 
+-- A program in raw form, holding just what you would know after parsing a single line.
 data ProgramDef = ProgramDef
     { name :: String 
     , weight :: Int
     , childPrograms :: [String]
     } deriving Show
 
+-- An entire tree of programs, including their names, weights, and children.
 type ProgramTree = Tree.Tree (String, Int)
 
 parseProgram :: Parsec String () ProgramDef
@@ -28,7 +30,7 @@ parseProgram = do
 -- Find the only program that isn't a child of another program
 findRoot :: [ProgramDef] -> String
 findRoot progs = head $ (name <$> progs) \\ allChildren
-    where allChildren = progs >>= childPrograms 
+    where allChildren = progs >>= childPrograms -- All child programs. Anyone not in here is the root.
 
 -- Constructs the tree of programs
 buildTree :: [ProgramDef] -> ProgramTree
@@ -47,22 +49,29 @@ programWeight (Tree.Node (_, weight) children) = weight + sum (programWeight <$>
 -- Given a list of programs, which ONE program has a different weight than the others, if any?
 uniqueWeight :: [ProgramTree] -> Maybe (ProgramTree, Int)
 uniqueWeight progs = do
-    single <- find (\l -> length l == 1) groups
+    single <- find (\l -> length l == 1) groups -- Which program is the only one with its weight?
     let incorrectProgram = fst . head $ single
     let childWeights = sum (programWeight <$> Tree.subForest incorrectProgram)
+    -- The correct weight is the correct total weight of this tree minus the weights of the child programs.
     return (incorrectProgram, correctTotalWeight - childWeights)
 
     where weights = programWeight <$> progs
           sameWeight (_, w1) (_, w2) = w1 == w2
-          groups = groupBy sameWeight (zip progs weights)
-          correctTotalWeight = snd . head . fromJust . find (\l -> length l /= 1) $ groups
+          groups = groupBy sameWeight (zip progs weights) -- The programs grouped by the total weights in their tree.
+          -- Steal the correct weight from a sibling program. Just make sure we aren't choosing the wrong one.
+          correctTotalWeight = snd . head . fromJust . find (\l -> length l /= 1) $ groups 
 
 -- Recurses down the program tree and finds the program with an incorrect weight, if any.
 findError :: ProgramTree -> Maybe (String, Int)
 findError prog = do 
     (unique, correct) <- uniqueWeight (Tree.subForest prog)
+    -- We now know that this tree contains the error.
+    -- Recurse down to find it.
     return $ case findError unique of
+        -- Found the error, return it
         Just s -> s
+        -- Interesting, we didn't find the error in our children. 
+        -- That means that WE are the error.
         Nothing -> (fst . Tree.rootLabel $ unique, correct)
 
 readPrograms :: IO [ProgramDef]
@@ -75,5 +84,7 @@ main :: IO ()
 main = do
     programs <- readPrograms
     let tree = buildTree programs
+    -- Part 1: Find the root
     print $ rootLabel tree
+    -- Part 2: Find the error
     print $ findError tree
