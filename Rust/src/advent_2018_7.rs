da64a5abe332a2810540c8a4470676937c45bcc9
Spencer Sellers <spencesellers@gmail.com>
2018-12-08 13:44:02 -0600

    Solve day 7 part 2

--- Rust/src/advent_2018_7.rs
+++ Rust/src/advent_2018_7.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 use std::collections::BTreeSet;
 use adventlib::AsciiValue;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 extern crate adventlib;
 extern crate regex;
@@ -107,33 +108,92 @@ fn p1(deps: &Dependencies) {
     println!("{}", answer);
 }
 
-fn get_time_to_complete(c: char, multiplier: u64) -> u64 {
-    AsciiValue::from_char(c).unwrap().uppercase_index().unwrap() as u64 * multiplier
+fn get_time_to_complete(c: char, additional: u64) -> u64 {
+    (AsciiValue::from_char(c).unwrap().uppercase_index().unwrap() as u64 + 1) + additional
+}
+
+struct Elves {
+    elves: HashMap<char, u64>,
+    max_elves: usize
+}
+
+impl Elves {
+    fn tick(&mut self) -> BTreeSet<char> {
+        for remaining in self.elves.values_mut() {
+            *remaining -= 1;
+        }
+
+        let mut complete = BTreeSet::new();
+
+        for elf in self.elves.iter() {
+            if *elf.1 == 0 {
+                complete.insert(*elf.0);
+            }
+        }
+
+        for completed in complete.iter() {
+            self.elves.remove(completed);
+        }
+
+        return complete;
+    }
+
+    fn can_assign(&self) -> bool {
+        self.elves.len() < self.max_elves
+    }
+
+    fn assign(&mut self, step: char, time: u64) {
+        if !self.can_assign() {
+            panic!("No more elves!");
+        }
+        self.elves.insert(step, time);
+    }
+
+    fn all_complete(&self) -> bool {
+        self.elves.is_empty()
+    }
+
+    fn are_working_on(&self, step: char) -> bool {
+        self.elves.contains_key(&step)
+    }
 }
 
 fn p2(deps: &Dependencies) {
-    const MAX_ELVES: usize = 2;
+    const MAX_ELVES: usize = 5;
+    const BASE_DIFFICULTY: u64 = 60;
 
     let mut completed: BTreeSet<char> = BTreeSet::new();
-    let mut elves: Vec<Option<(char, u64)>> = Vec::new();
+    let mut elves = Elves {
+        elves: HashMap::new(),
+        max_elves: MAX_ELVES
+    };
+
+    let mut seconds = 0;
 
     loop {
-        let no_deps = deps.ready_to_run(&completed);
-        let next = no_deps.iter().next();
-        match next {
-            Some(next) => {
-                answer.push(*next);
-                completed.insert(*next);
+        let ready = deps.ready_to_run(&completed);
+        for next_step in ready {
+            if !elves.can_assign() {
+                break;
             }
-            None => {
-                if elves.is_empty() {
-                    // Elves are done working and we have nothing left to do
-                    break;
-                }
-                // Else wait for elves to finish what they're doing.
+            if elves.are_working_on(next_step) {
+                continue;
             }
+            let time = get_time_to_complete(next_step, BASE_DIFFICULTY);
+            println!("Elf is assigned {} for {}", next_step, time);
+            elves.assign(next_step, time);
         }
+        if elves.all_complete() {
+            // Elves are done working and we have nothing left to do
+            break;
+        }
+        // Else wait for elves to finish what they're doing.
+        let elves_finished = elves.tick();
+        completed.extend(elves_finished.iter());
+        seconds += 1;
     }
+
+    println!("Seconds: {}", seconds);
 }
 
 fn main() {
