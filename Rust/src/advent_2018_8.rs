1e30d52418196104db0b59a016da75f365975e7a
Spencer Sellers <spencesellers@gmail.com>
2018-12-08 10:52:34 -0600

    Create day 8

---
+++ Rust/src/advent_2018_8.rs
@@ -0,0 +1,16 @@
+#![feature(drain_filter)] 
+use regex::Regex;
+use std::collections::HashSet;
+use std::collections::BTreeSet;
+
+extern crate adventlib;
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+fn main() {
+//    let dependencies: Vec<(char, char)> = adventlib::read_input_lines("input.txt").iter()
+//        .map(|line| parse_dependency(line))
+//        .collect();
+//
+//    p1(&dependencies);
+}
