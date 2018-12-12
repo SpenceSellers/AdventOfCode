1773c99089c82e2b1423be090da2c514c6291a0e
Spencer Sellers <ssellers@atlasrfid.com>
2018-12-11 22:39:33 -0600

    WIP window stuff

--- Rust/src/adventlib/grid/window.rs
+++ Rust/src/adventlib/grid/window.rs
@@ -8,6 +8,12 @@ pub struct GridWindow<G> {
 impl<G: GridView> GridView for GridWindow<G> {
     type Item = G::Item;
     fn get_cell(&self, pos: Point) -> Self::Item {
+        let adjusted = pos.subtract(self.region.lesser());
         self.grid.get_cell(pos)
     }
+}
+
+impl<G> DefinedSizeGrid for GridWindow<G> {
+    fn width(&self) -> usize { self.region.width() as usize }
+    fn height(&self) -> usize { self.region.height() as usize }
 }
