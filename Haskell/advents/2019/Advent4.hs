73687fb6167547d6cd6ef6bdacd34f24158c26a9
Spencer Sellers <ssellers@atlasrfid.com>
2019-12-04 12:28:31 -0600

    Solve 2019 day 4

--- Haskell/advents/2019/Advent4.hs
+++ Haskell/advents/2019/Advent4.hs
@@ -2,8 +2,10 @@ module Main where
 import AdventLib
 import Data.List
 
-isValid :: String -> Bool
-isValid s = (hasExactlyDouble s) && (neverDecrease 0 $ (digits s))
+type RegionLengthRequirement = Int -> Bool
+
+isValid :: String -> -> RegionLengthRequirement -> Bool
+isValid regionLength s = (hasExactlyDouble s) && (neverDecrease 0 $ (digits s))
 
 digits :: String -> [Int]
 digits s = read <$> digitStrings
@@ -13,8 +15,8 @@ hasDouble :: Eq a => [a] -> Bool
 hasDouble s = any (\[a, b] -> a == b) chunks
     where chunks = windows 2 s
 
-hasExactlyDouble :: Eq a => [a] -> Bool
-hasExactlyDouble l = any (==2) $ length <$> group l
+hasExactlyDouble :: Eq a => [a] -> RegionLengthRequirement -> Bool
+hasExactlyDouble l req = any req $ length <$> group l
 
 neverDecrease :: Int -> [Int] -> Bool
 neverDecrease biggest (x:xs) = if x >= biggest then neverDecrease x xs else False
@@ -23,4 +25,4 @@ neverDecrease _ [] = True
 main :: IO ()
 main = do
     let candidates = [152085..670283];
-    firstStar $ length $ filter isValid (show <$> candidates)
+    firstStar $ length $ filter isValid (==2) (show <$> candidates)
