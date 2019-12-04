0ee2436e0268b37e94a349d9df86dea4257ca587
Spencer Sellers <ssellers@atlasrfid.com>
2019-12-04 12:35:49 -0600

    Refactor 2019 day 4

--- Haskell/advents/2019/Advent4.hs
+++ Haskell/advents/2019/Advent4.hs
@@ -4,19 +4,15 @@ import Data.List
 
 type RegionLengthRequirement = Int -> Bool
 
-isValid :: String -> -> RegionLengthRequirement -> Bool
-isValid regionLength s = (hasExactlyDouble s) && (neverDecrease 0 $ (digits s))
+isValid :: RegionLengthRequirement -> String -> Bool
+isValid regionLength s = (hasRunOfLength regionLength s) && (neverDecrease 0 $ digits s)
 
 digits :: String -> [Int]
 digits s = read <$> digitStrings
     where digitStrings = (\x -> [x]) <$> s
 
-hasDouble :: Eq a => [a] -> Bool
-hasDouble s = any (\[a, b] -> a == b) chunks
-    where chunks = windows 2 s
-
-hasExactlyDouble :: Eq a => [a] -> RegionLengthRequirement -> Bool
-hasExactlyDouble l req = any req $ length <$> group l
+hasRunOfLength :: Eq a => RegionLengthRequirement -> [a] -> Bool
+hasRunOfLength req l = any req $ length <$> group l
 
 neverDecrease :: Int -> [Int] -> Bool
 neverDecrease biggest (x:xs) = if x >= biggest then neverDecrease x xs else False
@@ -24,5 +20,7 @@ neverDecrease _ [] = True
 
 main :: IO ()
 main = do
-    let candidates = [152085..670283];
-    firstStar $ length $ filter isValid (==2) (show <$> candidates)
+    firstStar $ solve (>= 2)
+    secondStar $ solve (== 2)
+    where solve req = length $ filter (isValid req) (show <$> candidates)
+          candidates = [152085..670283];
