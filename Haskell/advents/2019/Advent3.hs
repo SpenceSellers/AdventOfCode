36c147e1a4dbdf9cd2f993d867b8a34a3629b8d4
Spencer Sellers <ssellers@atlasrfid.com>
2019-12-04 16:24:58 -0600

    WIP on 2019 day 3. It works on the test input but not the real input :(

--- Haskell/advents/2019/Advent3.hs
+++ Haskell/advents/2019/Advent3.hs
@@ -6,22 +6,23 @@ import Text.Parsec.String
 import Parsing
 import Data.Maybe
 import Data.List
+import Debug.Trace
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
-buildMap :: Point -> [(Direction, Int)] -> Map.Map Point Int -> Map.Map Point Int
-buildMap pos [] existing = existing
-buildMap pos (x: xs) existing = buildMap (shiftPointBy pos direction n) xs nextMap
+buildMap :: Int -> Point -> [(Direction, Int)] -> Map.Map Point Int -> Map.Map Point Int
+buildMap step pos [] existing = existing
+buildMap step pos (x: xs) existing = buildMap (step + n) (shiftPointBy pos direction n) xs nextMap
     where (direction, n) = x
-          pointsCrossed = (\shift -> shiftPointBy pos direction shift) <$> [1..n] 
-          nextMap = foldr (\point m -> Map.alter (\c -> Just $ fromMaybe 1 ((+1) <$> c)) point m) existing pointsCrossed
-          
+          pointsCrossed = (\shift -> (shift, shiftPointBy pos direction shift)) <$> [1..n] 
+          nextMap = foldr (\(shift, point) m -> Map.insert point (step + shift) m) existing pointsCrossed 
+
 main :: IO ()
 main = do
     inputStrings <- inputLines
     let [Prelude.Right input1, Prelude.Right input2] = parse (commaSeparated letterNumber) "" <$> inputStrings
     let instructions = map (\(d, n) -> (fromJust $ readDirection [d], n)) <$> [input1, input2]
-    let completeMaps = (\insts -> buildMap (0, 0) insts Map.empty) <$> instructions
-    let [map1, map2] = fmap (Set.fromList . fmap fst . Map.toList) completeMaps
+    let completeMaps = (\insts -> buildMap 0 (0, 0) insts Map.empty) <$> instructions
+    let [map1, map2] = completeMaps
 
-    firstStar $ sortOn manhattanDistanceFromOrigin $ Set.toList $ Set.intersection map1 map2
+    firstStar $ sortOn snd $ Map.toList $ Map.intersectionWith (+) map1 map2
