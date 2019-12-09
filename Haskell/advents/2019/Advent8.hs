45c4cfbb4d88763835efb58eced208671980a75d
Spencer Sellers <spencesellers@gmail.com>
2019-12-08 18:50:41 -0600

    Solve day 8 part 2

--- Haskell/advents/2019/Advent8.hs
+++ Haskell/advents/2019/Advent8.hs
@@ -3,6 +3,11 @@ import AdventLib
 import Data.List
 import Data.List.Split
 
+mergeLayers :: [Int] -> [Int] -> [Int]
+mergeLayers = zipWith merger
+    where merger 2 b = b
+          merger a _ = a
+
 main :: IO ()
 main = do
     nums <- digits <$> inputFile
@@ -11,5 +16,10 @@ main = do
     let num1s = count 1 layerWithLeastZeroes
     let num2s = count 2 layerWithLeastZeroes
     firstStar $ num1s * num2s
+
+    let assembledLayers = foldr1 mergeLayers layers
+    -- And now just squint at this really hard
+    secondStar $ chunksOf width assembledLayers
+
     where width = 25
           height = 6
