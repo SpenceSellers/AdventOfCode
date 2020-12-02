72312ff3e248e1f6f7c8681f18ac4f6d5a6f9790
Spencer Sellers <spencesellers@gmail.com>
2020-12-01 20:35:22 -0600

    Prepare for part 2

---
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202002.cs
@@ -0,0 +1,19 @@
+namespace AdventOfCode.Days
+{
+    public class Advent202002 : Problem
+    {
+        public Advent202002() : base(2020, 02)
+        {
+        }
+
+        public override string PartOne(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string PartTwo(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
