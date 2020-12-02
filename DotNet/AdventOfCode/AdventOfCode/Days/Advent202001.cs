f92f06e36ad852ce9bc23c2553929c8b57d54d22
Spencer Sellers <spencesellers@gmail.com>
2020-12-01 18:50:17 -0600

    Poorly solve day 1

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202001.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202001.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace AdventOfCode.Days
 {
     public class Advent202001 : Problem
@@ -8,12 +10,48 @@ namespace AdventOfCode.Days
         
         public override string PartOne(string[] input)
         {
-            throw new System.NotImplementedException();
+            var nums = input.Select(int.Parse).ToList();
+            for (var i = 0; i < nums.Count; i++)
+            {
+                for (var j = 0; j < nums.Count; j++)
+                {
+                    if (i == j) continue;
+                    var ivalue = nums[i];
+                    var jvalue = nums[j];
+
+                    if (ivalue + jvalue == 2020)
+                    {
+                        return (ivalue * jvalue).ToString();
+                    }
+                }
+            }
+
+            return null;
         }
 
         public override string PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var nums = input.Select(int.Parse).ToList();
+            for (var i = 0; i < nums.Count; i++)
+            {
+                for (var j = 0; j < nums.Count; j++)
+                {
+                    for (var k = 0; k < nums.Count; k++)
+                    {
+                        if (i == j || i == k) continue;
+                        var ivalue = nums[i];
+                        var jvalue = nums[j];
+                        var kvalue = nums[k];
+
+                        if (ivalue + jvalue + kvalue == 2020)
+                        {
+                            return (ivalue * jvalue * kvalue).ToString();
+                        }
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
