3113462da3d83639eac0c36032ae5fd81c63a375
Spencer Sellers <spencesellers@gmail.com>
2020-12-01 18:11:47 -0600

    Add file for day 1

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202001.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202001.cs
@@ -1,7 +1,19 @@
 namespace AdventOfCode.Days
 {
-    public class Advent202001
+    public class Advent202001 : Problem
     {
+        public Advent202001() : base(2020, 01)
+        {
+        }
         
+        public override string PartOne(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string PartTwo(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- DotNet/AdventOfCode/AdventOfCode/Problem.cs
+++ DotNet/AdventOfCode/AdventOfCode/Problem.cs
@@ -1,7 +1,49 @@
+using System;
+using System.IO;
+using System.Net;
+
 namespace AdventOfCode
 {
-    public class Problem
+    public abstract class Problem
     {
+        public readonly int Year;
+        public readonly int Day;
         
+        public Problem(int year, int day)
+        {
+            Year = year;
+            Day = day;
+
+        }
+        
+        public abstract string PartOne(string[] input);
+        public abstract string PartTwo(string[] input);
+
+        public void Run()
+        {
+            var inputDir = Environment.GetEnvironmentVariable("INPUT_DIR");
+            var path = $"{inputDir}/input-{Year}-{Day}.txt";
+            var lines = File.ReadAllLines(path);
+
+            try
+            {
+                var result = PartOne(lines);
+                Console.WriteLine($"⭐ {result}");
+            }
+            catch (NotImplementedException e)
+            {
+                
+            }
+            
+            try
+            {
+                var result = PartTwo(lines);
+                Console.WriteLine($"⭐⭐ {result}");
+            }
+            catch (NotImplementedException e)
+            {
+                
+            }
+        }
     }
 }
