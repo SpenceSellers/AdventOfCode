d25237da5caa0e5b9c3bf662de0c95a7006f51d8
Spencer Sellers <spencesellers@gmail.com>
2020-12-12 18:55:39 -0600

    Actually complete the dreams of object oriented programmers for the first time in history

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202012.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202012.cs
@@ -13,54 +13,59 @@ namespace AdventOfCode.Days
         public override string PartOne(string[] input)
         {
             var ship = new Part1ShipState();
-            foreach (var (ins, arg) in input.Select(ParseInstruction))
-            {
-                ship.Apply(ins, arg);
-            }
-
-            return ship.Position.ManhattanDistanceFromOrigin().ToString();
+            return SolvePart(ship, input).ToString();
         }
 
         public override string PartTwo(string[] input)
         {
             var ship = new Part2ShipState();
+            return SolvePart(ship, input).ToString();
+        }
+
+        private int SolvePart(ShipState ship, string[] input)
+        {
             foreach (var (ins, arg) in input.Select(ParseInstruction))
             {
                 ship.Apply(ins, arg);
             }
 
-            return ship.Position.ManhattanDistanceFromOrigin().ToString();
+            return ship.Position.ManhattanDistanceFromOrigin();
         }
 
         private (char, int) ParseInstruction(string s) => (s[0], int.Parse(s.Substring(1)));
 
-        private class Part1ShipState
+        private abstract class ShipState
         {
             public GridPoint Position = new GridPoint(0, 0);
-            public int Facing;
+            public abstract void Apply(char instruction, int argument);
+        }
+        
+        private class Part1ShipState: ShipState
+        {
+            private int _facing;
 
             private void ApplyRotation(int degrees)
             {
                 // Hacky way to support the type of negative modulo we need
-                Facing += degrees + 360;
-                Facing %= 360;
+                _facing += degrees + 360;
+                _facing %= 360;
             }
 
             private void MoveForward(int distance)
             {
-                var direction = Facing switch
+                var direction = _facing switch
                 {
                     0 => 'E',
                     90 => 'S',
                     180 => 'W',
                     270 => 'N',
-                    _ => throw new Exception($"Invalid current facing: {Facing}")
+                    _ => throw new Exception($"Invalid current facing: {_facing}")
                 };
                 
                 Apply(direction, distance);
             }
-
-            public void Apply(char instruction, int argument)
+            
+            public override void Apply(char instruction, int argument)
             {
                 switch (instruction)
                 {
@@ -89,9 +94,8 @@ namespace AdventOfCode.Days
             }
         }
 
-        private class Part2ShipState
+        private class Part2ShipState : ShipState
         {
-            public GridPoint Position = new GridPoint(0, 0);
             public GridPoint WaypointPosition = new GridPoint(10, 1);
             // public int Facing;
             
@@ -105,7 +109,7 @@ namespace AdventOfCode.Days
                 Position += WaypointPosition.Scale(distance);
             }
             
-            public void Apply(char instruction, int argument)
+            public override void Apply(char instruction, int argument)
             {
                 switch (instruction)
                 {
