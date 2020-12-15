a0286da4fd061403204f59bf226ed55e843c7ff3
Spencer Sellers <spencesellers@gmail.com>
2020-12-14 22:15:31 -0600

    Solve day 14 pt 2

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202014.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202014.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Days
@@ -42,7 +43,86 @@ namespace AdventOfCode.Days
 
         public override string PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            long mask1 = 0;
+            long maskX = 0;
+            var values = new Dictionary<long, long>();
+            var lineRegex = new Regex(@"^(.*?) = (.*)$");
+            var memRegex = new Regex(@"mem\[(\d+)\]");
+            foreach (var line in input)
+            {
+                var match = lineRegex.Match(line);
+                var lvalue = match.Groups[1].Value;
+                var rvalue = match.Groups[2].Value;
+
+                var memGroup = memRegex.Match(lvalue);
+                if (memGroup.Success)
+                {
+                    var addr = long.Parse(memGroup.Groups[1].Value);
+                    var masked = addr | mask1;
+                    foreach (var address in MaskOptions(masked, maskX))
+                    {
+                        values[address] = long.Parse(rvalue);
+                    }
+                }
+                else
+                {
+                    mask1 = Convert.ToInt64(rvalue.Replace("X", "0"), 2);
+                    maskX = Convert.ToInt64(rvalue.Replace("1", "0").Replace("X", "1"), 2);
+                }
+            }
+
+            return values.Values.Sum().ToString();
         }
+
+        public IEnumerable<long> MaskOptions(long target, long allMask)
+        {
+            // I really wanted to do this with bit fiddling but by head hurts
+            var positions = PositionsFromMask(allMask);
+            for (long i = 0; i < 1 << positions.Count; i++)
+            {
+                yield return Emblazon(target, i, positions);
+            }
+        }
+
+        private static List<int> PositionsFromMask(long allMask)
+        {
+            var positions = new List<int>();
+            for (int j = 0; j <= 40; j++)
+            {
+                var mask = 1L << j;
+                if ((mask & allMask) != 0)
+                {
+                    positions.Add(j);
+                }
+            }
+
+            return positions;
+        }
+
+        public static long Emblazon(long target, long source, IEnumerable<int> positions)
+        {
+            var sourceIndex = 0;
+            foreach (var targetIndex in positions)
+            {
+                var sourceMask = 1L << sourceIndex;
+                // exactly 0 or 1
+                var sourceBit = (source & sourceMask) >> sourceIndex;
+                // 0000100000
+                var targetMask = 1L << targetIndex;
+                // Blank out that bit on the target
+                target &= ~targetMask;
+                
+                // Optionally fill it back in with a 1
+                if (sourceBit != 0)
+                {
+                    target |= targetMask;
+                }
+
+                sourceIndex++;
+            }
+
+            return target;
+        }
+        
     }
 }
