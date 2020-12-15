ea812484e7e8bc84633b79ef03ef38004986137a
Spencer Sellers <spencesellers@gmail.com>
2020-12-14 19:28:11 -0600

    Solve? part 2

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202013.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202013.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Linq;
+using System.Numerics;
+using System.Text;
 
 namespace AdventOfCode.Days
 {
@@ -23,13 +26,29 @@ namespace AdventOfCode.Days
                 .OrderBy(x => x.First)
                 .FirstOrDefault();
 
-
             return (timeToNext * idOfNext).ToString();
         }
 
         public override string PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var busses = input[1]
+                .Split(",")
+                .Select((b, i) => (b, i))
+                .Where(x => x.b != "x")
+                .Select(x => (int.Parse(x.b), x.i))
+                .ToList();
+
+            // Are ya winning, son?
+            var sb = new StringBuilder();
+            sb.AppendLine("So what you're going to do is go to this website:");
+            sb.AppendLine("https://www.dcode.fr/chinese-remainder");
+            sb.AppendLine("It's a pretty cute site. You'll then plug in these numbers:");
+            sb.AppendJoin('\n', busses.Select(x => $"[{x.Item1 - (x.i % x.Item1)}, {x.Item1}]"));
+            sb.AppendLine();
+            sb.AppendLine("That'll give you the answer.");
+
+            return sb.ToString();
         }
+
     }
 }
