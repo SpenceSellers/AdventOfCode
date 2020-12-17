4ce0ce9bee0952ec7eff0d3f5dd900195d73b2f6
Spencer Sellers <spencesellers@gmail.com>
2020-12-16 19:07:41 -0600

    Parse day 16

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202016.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202016.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Days
 {
@@ -10,7 +12,8 @@ namespace AdventOfCode.Days
 
         public override string PartOne(string[] input)
         {
-            throw new System.NotImplementedException();
+            var tickets = Parse(input);
+            return null;
         }
 
         public override string PartTwo(string[] input)
@@ -20,7 +23,30 @@ namespace AdventOfCode.Days
 
         private TicketSet Parse(string[] input)
         {
-            return null;
+            var chunks = input
+                .Where(line => line.Length != 0)
+                .SplitList(line => line == "your ticket:" || line == "nearby tickets:")
+                .ToList();
+
+            var ruleChunk = chunks[0];
+            var myTicketChunk = chunks[1][0].Split(',').Select(int.Parse).ToArray();
+            var nearbyTicketsChunk = chunks[2].Select(l => l.Split(',').Select(int.Parse).ToArray()).ToArray();
+
+            var ruleRegex = new Regex(@"(.*?): (\d+)-(\d+) or (\d+)-(\d+)");
+            var rules = new Dictionary<string, List<(int, int)>>();
+            foreach (var line in ruleChunk)
+            {
+                var match = ruleRegex.Match(line);
+                var ruleBody = new List<(int, int)>
+                {
+                    (int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value)),
+                    (int.Parse(match.Groups[4].Value), int.Parse(match.Groups[5].Value)),
+                };
+                
+                rules.Add(match.Groups[1].Value, ruleBody);
+            }
+            
+            return new TicketSet(rules, myTicketChunk, nearbyTicketsChunk);
         }
 
         private class TicketSet
@@ -35,8 +61,6 @@ namespace AdventOfCode.Days
                 MyTicket = myTicket;
                 NearbyTickets = nearbyTickets;
             }
-            
-            
         }
     }
 }
