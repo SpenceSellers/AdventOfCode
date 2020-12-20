7130bd87e35cde9af8b6004916158a6930e582e6
Spencer Sellers <spencesellers@gmail.com>
2020-12-20 12:01:31 -0600

    Records are freaking incredible

--- DotNet/AdventOfCode/AdventOfCode/Days/Advent202017.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/Advent202017.cs
@@ -17,14 +17,14 @@ namespace AdventOfCode.Days
             return new GridProblem4D().Solve(input);
         }
         
-        private static HashSet<Point3D> BuildInitialState(SolidGrid<bool> baseGrid)
+        private static HashSet<Point4D> BuildInitialState(SolidGrid<bool> baseGrid)
         {
-            var initialState = new HashSet<Point3D>();
+            var initialState = new HashSet<Point4D>();
             foreach (var point in baseGrid.Region().AllPoints())
             {
                 if (baseGrid.Get(point))
                 {
-                    var point3d = new Point3D(point.X, point.Y, 0);
+                    var point3d = new Point4D(point.X, point.Y, 0);
                     initialState.Add(point3d);
                 }
             }
@@ -34,7 +34,7 @@ namespace AdventOfCode.Days
 
         private abstract class GridProblem
         {
-            protected abstract IEnumerable<Point3D> AdjacentPoints(Point3D activePoint);
+            protected abstract IEnumerable<Point4D> AdjacentPoints(Point4D activePoint);
 
             public string Solve(string[] input)
             {
@@ -52,9 +52,9 @@ namespace AdventOfCode.Days
                 return state.Count.ToString();
             }
             
-            private HashSet<Point3D> PointsToConsider(HashSet<Point3D> grid)
+            private HashSet<Point4D> PointsToConsider(HashSet<Point4D> grid)
             {
-                var points = new HashSet<Point3D>();
+                var points = new HashSet<Point4D>();
                 foreach (var activePoint in grid)
                 {
                     points.Add(activePoint);
@@ -67,10 +67,10 @@ namespace AdventOfCode.Days
                 return points;
             }
 
-            private HashSet<Point3D> NextStep(HashSet<Point3D> grid)
+            private HashSet<Point4D> NextStep(HashSet<Point4D> grid)
             {
                 var pointsToConsider = PointsToConsider(grid);
-                var nextState = new HashSet<Point3D>();
+                var nextState = new HashSet<Point4D>();
                 foreach (var point in pointsToConsider)
                 {
                     var currentlyActive = grid.Contains(point);
@@ -92,21 +92,21 @@ namespace AdventOfCode.Days
 
         private class GridProblem3D : GridProblem
         {
-            protected override IEnumerable<Point3D> AdjacentPoints(Point3D point)
+            protected override IEnumerable<Point4D> AdjacentPoints(Point4D point)
             {
                 for (var dx = -1; dx <= 1; dx++)
                 for (var dy = -1; dy <= 1; dy++)
                 for (var dz = -1; dz <= 1; dz++)
                 {
                     if (dx == 0 && dy == 0 && dz == 0) continue;
-                    yield return new Point3D(point.X + dx, point.Y + dy, point.Z + dz);
+                    yield return new Point4D(point.X + dx, point.Y + dy, point.Z + dz);
                 }
             }
         }
 
         private class GridProblem4D : GridProblem
         {
-            protected override IEnumerable<Point3D> AdjacentPoints(Point3D point)
+            protected override IEnumerable<Point4D> AdjacentPoints(Point4D point)
             {
                 for (var dx = -1; dx <= 1; dx++)
                 for (var dy = -1; dy <= 1; dy++)
@@ -114,43 +114,11 @@ namespace AdventOfCode.Days
                 for (var dw = -1; dw <= 1; dw++)
                 {
                     if (dx == 0 && dy == 0 && dz == 0 && dw == 0) continue;
-                    yield return new Point3D(point.X + dx, point.Y + dy, point.Z + dz, point.W + dw);
+                    yield return new Point4D(point.X + dx, point.Y + dy, point.Z + dz, point.W + dw);
                 }
             }
         }
-        
-        private class Point3D
-        {
-            public readonly int X;
-            public readonly int Y;
-            public readonly int Z;
-            public readonly int W;
 
-            public Point3D(int x, int y, int z, int w=0)
-            {
-                X = x;
-                Y = y;
-                Z = z;
-                W = w;
-            }
-
-            protected bool Equals(Point3D other)
-            {
-                return X == other.X && Y == other.Y && Z == other.Z && W == other.W;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                if (obj.GetType() != this.GetType()) return false;
-                return Equals((Point3D) obj);
-            }
-
-            public override int GetHashCode()
-            {
-                return HashCode.Combine(X, Y, Z, W);
-            }
-        }
+        private record Point4D(int X, int Y, int Z, int W = 0);
     }
 }
