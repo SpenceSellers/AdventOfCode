d49211407a899c2fab517d82d740318849d48988
Spencer Sellers <spencesellers@gmail.com>
2021-12-01 17:52:51 -0600

    Auto-fetch puzzle input

--- DotNet/AdventOfCode/AdventOfCode/ProblemRunner.cs
+++ DotNet/AdventOfCode/AdventOfCode/ProblemRunner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 
 namespace AdventOfCode
 {
@@ -38,8 +39,8 @@ namespace AdventOfCode
         {
             var inputPath = FilePath(problem, "input");
             var samplePath = FilePath(problem, "sample");
-            EnsureFileExists(inputPath);
-            EnsureFileExists(samplePath);
+            EnsureFileExists(inputPath, () => SessionToken != null ? FetchInput(problem) : Array.Empty<byte>());
+            EnsureFileExists(samplePath, Array.Empty<byte>);
             var lines = File.ReadAllLines(_inputSource switch
             {
                 ProblemInputSource.Input => inputPath,
@@ -48,20 +49,6 @@ namespace AdventOfCode
 
             if (_runPartOne) RunProblem("⭐", () => problem.PartOne(lines)?.ToString());
             if (_runPartTwo) RunProblem("⭐⭐", () => problem.PartTwo(lines)?.ToString());
-        }
-
-        private string FilePath(Problem problem, string prefix)
-        {
-            var inputDir = Environment.GetEnvironmentVariable("INPUT_DIR") ?? Environment.CurrentDirectory;
-            var path = $"{inputDir}/{prefix}-{problem.Year}-{problem.Day}.txt";
-            return path;
-        }
-
-        private static void EnsureFileExists(string path)
-        {
-            if (File.Exists(path)) return;
-            using var file = File.Create(path);
-            Console.Out.WriteLine($"Created file at {path}");
         }
 
         private void RunProblem(string title, Func<string> func)
@@ -78,8 +65,46 @@ namespace AdventOfCode
             }
             catch (NotImplementedException e)
             {
-                
+
             }
         }
+
+        private string FilePath(Problem problem, string prefix)
+        {
+            var inputDir = Environment.GetEnvironmentVariable("INPUT_DIR") ?? Environment.CurrentDirectory;
+            var path = $"{inputDir}/{prefix}-{problem.Year}-{problem.Day}.txt";
+            return path;
+        }
+
+        private static void EnsureFileExists(string path, Func<byte[]> content)
+        {
+            if (File.Exists(path)) return;
+            using var file = File.Create(path);
+            file.Write(content());
+            Console.Out.WriteLine($"Created file at {path}");
+        }
+
+        private byte[] FetchInput(Problem problem)
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var request = new HttpRequestMessage();
+                request.Headers.Add("Cookie", $"session={SessionToken}");
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri($"https://adventofcode.com/{problem.Year}/day/{problem.Day}/input");
+
+                var response = client.Send(request);
+                Console.Out.WriteLine("Fetched puzzle input");
+                return response.Content.ReadAsByteArrayAsync().Result;
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("Failed to fetch puzzle input, creating empty file");
+                return new byte[] { };
+            }
+        }
+
+        private static string? SessionToken => Environment.GetEnvironmentVariable("SESSION_TOKEN");
     }
 }
