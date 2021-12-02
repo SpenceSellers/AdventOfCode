3cc6de9b2a56be637b1bea5ba3dddcff439d0b4e
Spencer Sellers <spencesellers@gmail.com>
2021-12-01 23:09:32 -0600

    Day 2 initial solution

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202102.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202102.cs
@@ -1,15 +1,57 @@
+using System.Linq;
+
 namespace AdventOfCode.Days._2021
 {
     public class Advent202102 : Problem
     {
         public override object PartOne(string[] input)
         {
-            throw new System.NotImplementedException();
+            var instructions = input.Select(x => x.Split()).Select(x => (x[0], int.Parse(x[1])));
+            var x = 0;
+            var y = 0;
+            foreach (var (direction, distance) in instructions)
+            {
+                switch (direction)
+                {
+                    case "forward":
+                        x += distance;
+                        break;
+                    case "down":
+                        y += distance;
+                        break;
+                    case "up":
+                        y -= distance;
+                        break;
+                }
+            }
+
+            return x * y;
         }
 
         public override object PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var instructions = input.Select(x => x.Split()).Select(x => (x[0], int.Parse(x[1])));
+            var x = 0;
+            var y = 0;
+            var aim = 0;
+            foreach (var (direction, distance) in instructions)
+            {
+                switch (direction)
+                {
+                    case "forward":
+                        x += distance;
+                        y += distance * aim;
+                        break;
+                    case "down":
+                        aim += distance;
+                        break;
+                    case "up":
+                        aim -= distance;
+                        break;
+                }
+            }
+
+            return x * y;
         }
     }
 }
