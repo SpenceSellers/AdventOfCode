a4c12f9efbdf4df29d74a8449764f51ac6d7896e
Spencer Sellers <spencesellers@gmail.com>
2021-12-02 23:32:02 -0600

    Solve part 2

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202103.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202103.cs
@@ -9,17 +9,24 @@ namespace AdventOfCode.Days._2021
         public override object PartOne(string[] input)
         {
             var bits = input.Select(x => x.ToCharArray()).ToList();
-            var length = input[0].Length;
-            var commonBits = Transpose(length, bits)
-                .Select(column =>
-                {
-                    var ones = column.Count(x => x == '1');
-                    return ones > (bits.Count / 2) ? '1' : '0';
-                }).ToList();
+            var commonBits = CommonBits(bits);
 
             var a =  Convert.ToInt32(string.Join("", commonBits), 2);
             var b =  Convert.ToInt32(Invert(string.Join("", commonBits)), 2);
             return a * b;
+        }
+
+        private static List<char> CommonBits(List<char[]> bits)
+        {
+            var length = bits[0].Length;
+            var commonBits = Transpose(length, bits)
+                .Select(column =>
+                {
+                    var ones = column.Count(x => x == '1');
+                    var zeroes = column.Count(x => x == '0');
+                    return ones >= zeroes ? '1' : '0';
+                }).ToList();
+            return commonBits;
         }
 
         private static IEnumerable<IEnumerable<char>> Transpose(int length, List<char[]> bits)
@@ -31,14 +38,52 @@ namespace AdventOfCode.Days._2021
             }
         }
 
-        public static string Invert(string s)
+        private static string Invert(string s)
         {
             return string.Join("", s.ToCharArray().Select(x => x == '1' ? '0' : '1'));
         }
 
         public override object PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var bits = input.Select(x => x.ToCharArray()).ToList();
+            var originalBits = bits;
+
+            for (var i = 0; i < bits[0].Length; i++)
+            {
+                var commonBits = CommonBits(bits);
+                var criticalBit = commonBits[i];
+                bits = bits.Where(c => c[i] == criticalBit).ToList();
+                if (bits.Count == 1)
+                {
+                    break;
+                }
+            }
+
+            if (bits.Count != 1)
+            {
+                throw new Exception("Uh oh " + bits.Count);
+            }
+            var aaa = Convert.ToInt32(string.Join("", bits[0]), 2);
+
+            bits = originalBits;
+            for (var i = 0; i < bits[0].Length; i++)
+            {
+                var commonBits = CommonBits(bits);
+                var criticalBit = commonBits[i] == '1' ? '0' : '1';
+                bits = bits.Where(c => c[i] == criticalBit).ToList();
+                if (bits.Count == 1)
+                {
+                    break;
+                }
+            }
+
+            if (bits.Count != 1)
+            {
+                throw new Exception("Uh oh " + bits.Count);
+            }
+
+            var bbb = Convert.ToInt32(string.Join("", bits[0]), 2);
+            return aaa * bbb;
         }
     }
 }
--- DotNet/AdventOfCode/AdventOfCode/Program.cs
+++ DotNet/AdventOfCode/AdventOfCode/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             new ProblemRunner()
-                .UseSampleInput()
+                // .UseSampleInput()
                 // .SkipPartOne()
                 .Run(new Advent202103());
         }
