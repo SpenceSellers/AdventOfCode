c4f5f6ef010429037480dbe438f4a654de3db712
Spencer Sellers <spencesellers@gmail.com>
2021-12-08 00:45:43 -0600

    Delete my hopes and dreams

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202108.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202108.cs
@@ -10,20 +10,25 @@ namespace AdventOfCode.Days._2021
 
         public override object PartOne(string[] input)
         {
+            return ParseInput(input)
+                .SelectMany(line =>
+                {
+                    var examples = line[0];
+                    var digits = line[1];
+                    var mapping = BuildSimplestMappings(examples);
+                    return digits.Select(d => mapping.GetValueOrDefault(d, -1));
+                }).Count(d => d is 1 or 4 or 7 or 8);
+        }
+
+        private List<List<List<string>>> ParseInput(string[] input)
+        {
             var parsed = input
                 .Select(line => line.Split('|', StringSplitOptions.TrimEntries)
                     .Select(side => side
                         .Split()
                         .Select(NormalizeString)))
                 .Realize();
-
-            return parsed.SelectMany(line =>
-            {
-                var examples = line[0];
-                var digits = line[1];
-                var mapping = BuildSimplestMappings(examples);
-                return digits.Select(d => mapping.GetValueOrDefault(d, -1));
-            }).Count(d => d is 1 or 4 or 7 or 8);
+            return parsed;
         }
 
         private string NormalizeString(string s)
@@ -47,42 +52,8 @@ namespace AdventOfCode.Days._2021
             return mapping;
         }
 
-        private void ResolveMappings(List<string> scrambled)
-        {
 
-            var resolver = new LetterResolver();
-            // Seed the possible mappings by knocking out the ones we KNOW can't be true
-            foreach (var (simpleMapping, i) in BuildSimplestMappings(scrambled))
-            {
-                resolver.KnownCharacter(simpleMapping, i);
-            }
-        }
-
-        public IEnumerable<Dictionary<char, char>> AllPossibleMappings()
-        {
-            return "abcdefg"
-                .ToList()
-                .Permutations()
-                .Select(scrambled => "abcdefg"
-                    .Zip(scrambled)
-                    .ToDictionary(k => k.Second, v => v.First));
-        }
-
-        private Dictionary<int, string> _digitTable = new()
-        {
-            {0, "abcefg"},
-            {1, "cf"},
-            {2, "acdeg"},
-            {3, "acdfg"},
-            {4, "bcdf"},
-            {5, "abdfg"},
-            {6, "abdefg"},
-            {7, "acf"},
-            {8, "abcdefg"},
-            {9, "abcdfg"},
-        };
-
-        private Dictionary<string, int> _inverseDigitTable = new()
+        private readonly Dictionary<string, int> _digitTable = new()
         {
             {"abcefg", 0},
             {"cf", 1},
@@ -98,88 +69,47 @@ namespace AdventOfCode.Days._2021
 
         public override object PartTwo(string[] input)
         {
-            var parsed = input
-                .Select(line => line.Split('|', StringSplitOptions.TrimEntries)
-                    .Select(side => side
-                        .Split()
-                        .Select(NormalizeString)))
-                .Realize();
-
-            return parsed.Select((line, i) =>
+            return ParseInput(input).Select((line, i) =>
             {
                 Console.Out.WriteLine($"Line {i}");
                 var examples = line[0];
                 var digits = line[1];
-                var mapping = DetermineMapping(examples);
-                var solved = digits.Select(d => _inverseDigitTable[NormalizeString(ApplyMapping(mapping, d))]);
+                var mapping = BruteForceMapping(examples);
+                var solved = digits.Select(d => _digitTable[NormalizeString(ApplyMapping(mapping, d))]);
                 return int.Parse(string.Join("", solved.Select(i => i.ToString())));
             }).Sum();
         }
 
-        private Dictionary<char, char> DetermineMapping(List<string> examples)
+        private Dictionary<char, char> BruteForceMapping(List<string> examples)
         {
             return AllPossibleMappings()
                 .Where(mapping => MappingIsPossible(mapping, examples))
                 .ToList().First();
         }
+        
+        // Yep, we're seriously going to brute force everything the answer could be.
+        private IEnumerable<Dictionary<char, char>> AllPossibleMappings() {
+            return "abcdefg"
+                .ToList()
+                .Permutations()
+                .Select(scrambled => "abcdefg"
+                    .Zip(scrambled)
+                    .ToDictionary(k => k.Second, v => v.First));
+        }
 
-        public string ApplyMapping(Dictionary<char, char> mapping, string scrambled)
+        private string ApplyMapping(Dictionary<char, char> mapping, string scrambled)
         {
             return string.Join("", scrambled.Select(c => mapping[c]));
         }
 
-        public bool MappingIsPossible(Dictionary<char, char> mapping, List<string> examples)
+        private bool MappingIsPossible(Dictionary<char, char> mapping, List<string> examples)
         {
             var mapped = examples
                 .Select(example => ApplyMapping(mapping, example))
-                .Select(mapped => _inverseDigitTable.GetValueOrDefault(NormalizeString(mapped), -1))
+                .Select(mapped => _digitTable.GetValueOrDefault(NormalizeString(mapped), -1))
                 .Where(x => x is not -1)
                 .ToList();
             return mapped.ToHashSet().Count == 10;
         }
-    }
-
-    // I literally gave up on doing it the right way
-    public class LetterResolver
-    {
-        private Dictionary<int, string> _digitTable = new()
-        {
-            {0, "abcefg"},
-            {1, "cf"},
-            {2, "acdeg"},
-            {3, "acdfg"},
-            {4, "bcdf"},
-            {5, "abdfg"},
-            {6, "abdefg"},
-            {7, "acf"},
-            {8, "abcdefg"},
-            {9, "abcdfg"},
-        };
-
-        // Maps Scrambled characters <-> Real characters
-        private Dictionary<char, List<char>> _possibilities = new();
-
-        public LetterResolver()
-        {
-            foreach (var c in "abcdefg")
-            {
-                _possibilities[c] = "abcdefg".ToList();
-            }
-        }
-
-        public void KnownCharacter(string scrambled, int n)
-        {
-            var realDigits = _digitTable[n];
-            foreach (var scrambledChar in scrambled.ToList())
-            {
-                _possibilities[scrambledChar].RemoveAll(c => !realDigits.Contains(c));
-            }
-        }
-
-        // public IEnumerable<Dictionary<char, char>> AllPossibleMappings(List<char> decided = null)
-        // {
-        //     decided ??= new List<char>();
-        //
-        // }
     }
 }
