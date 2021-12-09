c55a1c3bda8653869d09b316886747969ee9b383
Spencer Sellers <spencesellers@gmail.com>
2021-12-08 23:10:14 -0600

    Solve part 1

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202109.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202109.cs
@@ -1,10 +1,25 @@
+using System.Linq;
+using AdventOfCode.AdventLib;
+using AdventOfCode.AdventLib.Grid;
+
 namespace AdventOfCode.Days._2021
 {
     public class Advent202109 : Problem
     {
         public override object PartOne(string[] input)
         {
-            throw new System.NotImplementedException();
+            var grid = input.ToGrid().Map(x => int.Parse(x.ToString()));
+            var dangerSpots = grid.Region().AllPoints().Where(point =>
+            {
+                var height = grid.Get(point);
+                return point.Adjacent4
+                    .Where(adjacentPoint => grid.Region().ContainsPoint(adjacentPoint))
+                    .All(adjacentPoint => grid.Get(adjacentPoint) > height);
+            });
+
+            return dangerSpots
+                .Select(x => grid.Get(x) + 1)
+                .Sum();
         }
 
         public override object PartTwo(string[] input)
