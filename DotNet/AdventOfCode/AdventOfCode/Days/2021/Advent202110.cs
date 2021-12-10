4160d055f1eac5d1a9d7ad98fed2a9c7520be53a
Spencer Sellers <spencesellers@gmail.com>
2021-12-09 23:23:04 -0600

    Part 2

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202110.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202110.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using AdventOfCode.AdventLib;
 
 namespace AdventOfCode.Days._2021
 {
     public class Advent202110 : Problem
     {
+
         public override object PartOne(string[] input)
         {
             var results = input.Select(FirstIncorrectChar).ToList();
@@ -20,7 +23,7 @@ namespace AdventOfCode.Days._2021
             '>' => 25137
         };
 
-        public char? FirstIncorrectChar(string line)
+        private char? FirstIncorrectChar(string line)
         {
             var stack = new Stack<char>();
             foreach (var c in line)
@@ -51,9 +54,67 @@ namespace AdventOfCode.Days._2021
             return null;
         }
 
+        private List<char> FirstIncorrectChar2(string line)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in line)
+            {
+                char? closer = c switch
+                {
+                    '(' => ')',
+                    '[' => ']',
+                    '<' => '>',
+                    '{' => '}',
+                    _ => null
+                };
+
+                if (closer is not null)
+                {
+                    stack.Push(closer.Value);
+                }
+                else
+                {
+                    var correctCloser = stack.Pop();
+                    if (c != correctCloser)
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return stack.ToList();
+        }
+
+        private long ScorePart2(IEnumerable<char> chars)
+        {
+            var sum = 0L;
+            foreach (var c in chars)
+            {
+                var charScore = c switch
+                {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4
+                };
+
+                sum *= 5;
+                sum += charScore;
+            }
+
+            return sum;
+        }
+
         public override object PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var scores = input
+                .Select(FirstIncorrectChar2)
+                .Where(x => x != null)
+                .Select(ScorePart2)
+                .OrderBy(x => x)
+                .ToList();
+
+            return scores[scores.Count / 2];
         }
     }
 }
