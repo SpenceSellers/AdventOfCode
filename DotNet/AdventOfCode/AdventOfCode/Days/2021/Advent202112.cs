36a4141870d25fdfec4f7590006e8bb0d4931bb6
Spencer Sellers <spencesellers@gmail.com>
2021-12-12 00:32:41 -0600

    Solve part 2

--- DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202112.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2021/Advent202112.cs
@@ -8,11 +8,7 @@ namespace AdventOfCode.Days._2021
     {
         public override object PartOne(string[] input)
         {
-            var edges = input
-                .Select(line => line.Split("-"))
-                .Select(split => (split[0], split[1]))
-                .SelectMany(a => new [] {a, (a.Item2, a.Item1)})
-                .ToList();
+            var edges = ParseDirectedEdges(input);
 
             var bfsConfig = new BfsSearchConfig<List<string>>
             {
@@ -35,11 +31,68 @@ namespace AdventOfCode.Days._2021
             return paths.Count;
         }
 
-        private bool IsLargeCave(string cave) => char.IsUpper(cave.First());
-
         public override object PartTwo(string[] input)
         {
-            throw new System.NotImplementedException();
+            var edges = ParseDirectedEdges(input);
+
+            var bfsConfig = new BfsSearchConfig<List<string>>
+            {
+                InitialState = new List<string> { "start" },
+                IsSuccessState = path => path.Last() == "end",
+                NextStates = (path) =>
+                {
+                    var last = path.Last();
+                    var smallCavesVisited = path
+                        .Where(cave => cave is not "start" or "end")
+                        .Where(cave => !IsLargeCave(cave))
+                        .ToList();
+                    var haveVisitedSmallCaveTwice = smallCavesVisited.Count != smallCavesVisited.ToHashSet().Count;
+                    var possibleWaysToGo = edges
+                        .Where1(cave => cave == last)
+                        .Select2()
+                        .Where(cave =>
+                        {
+                            if (IsLargeCave(cave))
+                            {
+                                return true;
+                            }
+
+                            if (haveVisitedSmallCaveTwice)
+                            {
+                                return !path.Contains(cave);
+                            }
+                            else
+                            {
+                                if (cave is "start" or "end")
+                                {
+                                    return !path.Contains(cave);
+                                }
+                                else
+                                {
+                                    return true;
+                                }
+                            }
+                        });
+
+                    return possibleWaysToGo.Select(next => path.Append(next).ToList());
+                }
+            };
+
+            var bfs = new BreadthFirstSearch<List<string>>(bfsConfig);
+            var paths = bfs.Bfs().ToList();
+            return paths.Count;
         }
+
+        private static List<(string, string)> ParseDirectedEdges(string[] input)
+        {
+            var edges = input
+                .Select(line => line.Split("-"))
+                .Select(split => (split[0], split[1]))
+                .SelectMany(a => new[] { a, (a.Item2, a.Item1) })
+                .ToList();
+            return edges;
+        }
+
+        private bool IsLargeCave(string cave) => char.IsUpper(cave.First());
     }
 }
