2c878a712628e06033eaf05f532cfe29ab6f422b
Spencer Sellers <spencesellers@gmail.com>
2022-11-20 16:59:06 -0600

    Enable resizing DoubleEndedList

--- DotNet/AdventOfCode/AdventOfCode/AdventLib/DoubleEndedList.cs
+++ DotNet/AdventOfCode/AdventOfCode/AdventLib/DoubleEndedList.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.AdventLib;
 
@@ -10,13 +11,15 @@ namespace AdventOfCode.AdventLib;
 public class DoubleEndedList<T> : IEnumerable<T>
 {
     private T[] _buffer;
-    private int _start = 0; // first index that actually contains an item
-    private int _end = 0; // Index of first empty item
-
-    private const int ResizingFactor = 2;
+    private int _start = 0;
+    private int _end = 0;
 
     public DoubleEndedList(int initialCapacity = 1)
     {
+        if (initialCapacity <= 0)
+        {
+            throw new InvalidOperationException($"Invalid initialCapacity of {initialCapacity}");
+        }
         _buffer = new T[initialCapacity];
     }
 
@@ -57,12 +60,16 @@ public class DoubleEndedList<T> : IEnumerable<T>
         }
     }
 
-    // -1 because a completely fully array is indistinguishable from an empty one given our current indexing strategy.
+    // -1 because a completely full array is indistinguishable from an empty one given our current indexing strategy.
     // This mean does that one array slot is wasted no matter what...
     private int RemainingCapacity => _buffer.Length - Count - 1;
 
     public T PopBack()
     {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("DoubleEndedList is empty, cannot pop");
+        }
         _end = Forwards(_end);
         var val = _buffer[_end];
         _buffer[_end] = default;
@@ -71,6 +78,10 @@ public class DoubleEndedList<T> : IEnumerable<T>
 
     public T PopFront()
     {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("DoubleEndedList is empty, cannot pop");
+        }
         var val = _buffer[_start];
         _buffer[_start] = default;
         _start = Backwards(_start);
@@ -79,22 +90,96 @@ public class DoubleEndedList<T> : IEnumerable<T>
 
     public void PushFront(T value)
     {
-        if (RemainingCapacity == 0)
-        {
-            Resize();
-        }
+        Reserve(1);
         _start = Forwards(_start);
         _buffer[_start] = value;
     }
 
     public void PushBack(T value)
     {
-        if (RemainingCapacity == 0)
-        {
-            Resize();
-        }
+        Reserve(1);
         _buffer[_end] = value;
         _end = Backwards(_end);
+    }
+
+    public void AddRangeBack(IEnumerable<T> range)
+    {
+        if (range.TryGetNonEnumeratedCount(out var count))
+        {
+            Reserve(count);
+        }
+
+        foreach (var v in range)
+        {
+            PushBack(v);
+        }
+    }
+
+    /// <summary>
+    /// Inserting A, B, C onto D, E, F yields A B C D E F
+    /// </summary>
+    public void AddRangeFront(IEnumerable<T> range)
+    {
+        if (range.TryGetNonEnumeratedCount(out var count))
+        {
+            Reserve(count);
+        }
+
+        foreach (var v in range.Reverse())
+        {
+            PushFront(v);
+        }
+    }
+
+    /// <summary>
+    /// Inserting A, B, C onto D, E, F yields C B A D E F, equivalent to successively pushing the items onto the front.
+    /// </summary>
+    public void AddRangeFrontMirrored(IEnumerable<T> range)
+    {
+        if (range.TryGetNonEnumeratedCount(out var count))
+        {
+            Reserve(count);
+        }
+
+        foreach (var v in range)
+        {
+            PushFront(v);
+        }
+    }
+    
+    /// <summary>
+    /// Ensure that we can fit at least N additional elements without resizing
+    /// </summary>
+    public void Reserve(int additional)
+    {
+        if (additional <= RemainingCapacity) return;
+
+        var nextSize = NextPowerOfTwo(_buffer.Length + additional);
+        ResizeTo(nextSize);
+    }
+
+    // This doesn't do any bounds checking so be sure you know what you're doing.
+    private void ResizeTo(int newSize)
+    {
+        var newBuffer = new T[newSize];
+        var i = 0;
+        foreach (var val in this)
+        {
+            newBuffer[i] = val;
+            i++;
+        }
+
+        _buffer = newBuffer;
+        _start = 0;
+        _end = i;
+    }
+
+    private static int NextPowerOfTwo(int n)
+    {
+        var power = 1;
+        while(power < n)
+            power*=2;
+        return power;
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -126,21 +211,5 @@ public class DoubleEndedList<T> : IEnumerable<T>
     private int Forwards(int x, int distance = 1)
     {
         return (x - distance + _buffer.Length) % _buffer.Length;
-    }
-
-    private void Resize()
-    {
-        var nextSize =_buffer.Length * ResizingFactor;
-        var newBuffer = new T[nextSize];
-        var i = 0;
-        foreach (var val in this)
-        {
-            newBuffer[i] = val;
-            i++;
-        }
-
-        _buffer = newBuffer;
-        _start = 0;
-        _end = i;
     }
 }
