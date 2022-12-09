a77d854d6d32a7d2adcad5e418c75b5c6c324358
Spence Sellers <spencesellers@gmail.com>
2022-12-08 21:07:41 -0600

    Refactor part 1

--- DotNet/AdventOfCode/AdventOfCode/Days/2022/Advent202208.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2022/Advent202208.cs
@@ -7,75 +7,23 @@ namespace AdventOfCode.Days._2022;
 
 public class Advent202208 : Problem
 {
+    private readonly GridDirection[] _allGridDirections = { GridDirection.Down, GridDirection.Up, GridDirection.Left, GridDirection.Right };
+
     public override object PartOne(string[] input)
     {
         var grid = input.ToGrid().Map(x => int.Parse(x.ToString()));
-
-        var visible = new SolidGrid<bool>(grid.Region().Width, grid.Region().Width, false);
-
-        for (var row = 0; row < grid.Height; row++)
-        {
-            var maxSoFar = -1;
-            for (var x = 0; x < grid.Width; x++)
+        var region = grid.Region();
+        return region.AsCoordinateGrid.Map(p =>
             {
-                var point = new GridPoint(x, row);
-                var heightHere = grid.Get(point);
-                if (heightHere > maxSoFar)
-                {
-                    visible.Set(point, true);
-                    maxSoFar = heightHere;
-                }
-            }
-        }
-
-        for (var row = 0; row < grid.Height; row++)
-        {
-            var maxSoFar = -1;
-            for (var x = 0; x < grid.Width; x++)
-            {
-                var point = new GridPoint(grid.Width - x - 1, row);
-                var heightHere = grid.Get(point);
-                if (heightHere > maxSoFar)
-                {
-                    visible.Set(point, true);
-                    maxSoFar = heightHere;
-                }
-            }
-        }
-
-        for (var col = 0; col < grid.Width; col++)
-        {
-            var maxSoFar = -1;
-            for (var y = 0; y < grid.Height; y++)
-            {
-                var point = new GridPoint(col, grid.Height - y - 1);
-                var heightHere = grid.Get(point);
-                if (heightHere > maxSoFar)
-                {
-                    visible.Set(point, true);
-                    maxSoFar = heightHere;
-                }
-            }
-        }
-
-        for (var col = 0; col < grid.Width; col++)
-        {
-            var maxSoFar = -1;
-            for (var y = 0; y < grid.Height; y++)
-            {
-                var point = new GridPoint(col, y);
-                var heightHere = grid.Get(point);
-                if (heightHere > maxSoFar)
-                {
-                    visible.Set(point, true);
-                    maxSoFar = heightHere;
-                }
-            }
-        }
-
-        return visible.Trace().AllCells().Count(x => x);
+                var ourHeight = grid.Get(p);
+                return _allGridDirections.Any(direction => March(p,
+                        direction,
+                        region)
+                    .All(marchPoint => grid.Get(marchPoint) < ourHeight));
+            })
+            .AllCells()
+            .Count(x => x);
     }
-
 
     public override object PartTwo(string[] input)
     {
@@ -84,7 +32,7 @@ public class Advent202208 : Problem
         var sightLines = region.AsCoordinateGrid.Map(p =>
         {
             var ourHeight = grid.Get(p);
-            return new[] { GridDirection.Down, GridDirection.Up, GridDirection.Left, GridDirection.Right }
+            return _allGridDirections
                 .Select(direction =>
                 {
                     var march = March(p, direction, region).ToList();
@@ -93,9 +41,13 @@ public class Advent202208 : Problem
                 .Product();
         });
 
-        return sightLines.AllCells().Max();
+        return sightLines.AllCells()
+            .Max();
     }
 
+    /// <summary>
+    /// Yield points successively further and further in a direction until we leave the defined region
+    /// </summary>
     private IEnumerable<GridPoint> March(GridPoint initial, GridDirection direction, GridRegion region)
     {
         var current = initial;
