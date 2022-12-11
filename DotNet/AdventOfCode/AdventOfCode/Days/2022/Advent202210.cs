6817c6909db1da354103d2d3306afa33bcceddef
Spence Sellers <spencesellers@gmail.com>
2022-12-10 23:11:08 -0600

    solve day 10 part 2

--- DotNet/AdventOfCode/AdventOfCode/Days/2022/Advent202210.cs
+++ DotNet/AdventOfCode/AdventOfCode/Days/2022/Advent202210.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using AdventOfCode.AdventLib.Grid;
 
 namespace AdventOfCode.Days._2022;
 
@@ -49,6 +51,43 @@ public class Advent202210 : Problem
 
     public override object PartTwo(string[] input)
     {
-        throw new System.NotImplementedException();
+        var x = 1;
+        var cycle = 0;
+        var score = 0;
+        var display = new SolidGrid<bool>(40, 10, false);
+        foreach (var line in input)
+        {
+            int cycleSpan;
+            var toAdd = 0;
+            if (line == "noop")
+            {
+                cycleSpan = 1;
+            }
+            else
+            {
+                toAdd = int.Parse(line.Split()[1]);
+                cycleSpan = 2;
+            }
+
+            for (int i = 0; i < cycleSpan; i++)
+            {
+                var sampledCycle = cycle + cycleSpan - i;
+                var cyclePos = sampledCycle % 40;
+                var row = sampledCycle / 40;
+
+                // Why minus one? I have no idea. Is it because cycles are supposed to be 1-indexed?
+                // Changing the first cycle to one doesn't help at all.
+                if (Math.Abs(cyclePos - x - 1 ) <= 1)
+                {
+                    display.Set(new GridPoint(cyclePos, row), true);
+                }
+            }
+
+            x += toAdd;
+            cycle += cycleSpan;
+        }
+
+        display.Trace();
+        return null;
     }
 }
